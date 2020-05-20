41853c6f758e4e612bacb74b6dcf9c3b3ffb9723
Micah Evans <micah.s.evans@gmail.com>
2020-05-20 14:59:26 -0400

    Added MONGODB_URI to server.js

--- server.js
+++ server.js
@@ -15,10 +15,13 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
+
 
 // routes
 app.use(require("./routes/api.js"));
